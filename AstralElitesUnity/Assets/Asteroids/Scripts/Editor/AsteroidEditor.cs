42967b3125abbd066eeb926d995cca2193cd6b8d
fydar <fydar.dev@gmail.com>
2020-08-06 00:05:20 +0100

    🚧 Added editor tool for testing asteroid generation

---
+++ AstralElitesUnity/Assets/Asteroids/Scripts/Editor/AsteroidEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Asteroid))]
+public class AsteroidEditor : Editor
+{
+	private static AsteroidTemplate asteroidTemplate;
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+		asteroidTemplate = (AsteroidTemplate)EditorGUILayout.ObjectField(new GUIContent("Preview Template"),
+			asteroidTemplate, typeof(AsteroidTemplate), false);
+
+		EditorGUI.BeginDisabledGroup(asteroidTemplate == null);
+		if (GUILayout.Button("Preview"))
+		{
+			var asteroid = (Asteroid)target;
+
+			asteroid.Generate(asteroidTemplate);
+		}
+		EditorGUI.EndDisabledGroup();
+		EditorGUILayout.EndVertical();
+	}
+}
